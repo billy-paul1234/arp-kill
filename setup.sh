b98be67b3dc67aedea74bb448a122a3f66b5d544
Billy Paul <137751689+billy-paul1234@users.noreply.github.com>
2023-09-19 15:33:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@
 chmod +x arp-kill     
 cp -f arp-kill /bin                                                                                            
 chmod +x /bin/arp-kill
+
+echo "sudo arp-kill <interface>"
