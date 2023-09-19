9b9bfa183a1c1d165ee350e9a7bc2699889eaabf
Billy Paul <137751689+billy-paul1234@users.noreply.github.com>
2023-09-19 15:32:07 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# script to setup arp-kill
+
+
+chmod +x arp-kill     
+cp -f arp-kill /bin                                                                                            
+chmod +x /bin/arp-kill
